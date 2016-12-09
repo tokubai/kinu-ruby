0f5a9717b1c967677da6a7b39b15cf02e1684c8a
Tomoki Yamashita <tomorrowkey@gmail.com>
2016-12-09 15:56:20 +0900

    Add kinu_spec

--- spec/kinu_spec.rb
+++ spec/kinu_spec.rb
@@ -1,7 +1,50 @@
 require 'spec_helper'
+require 'uri'
 
 RSpec.describe Kinu do
-  it 'passes' do
-    expect(1).to eq(1)
+
+  before do
+    Kinu.class_eval { @config = nil }
+  end
+
+  describe '.base_uri' do
+
+    subject { Kinu.base_uri }
+
+    context 'when configure empty hostname' do
+      it 'raises error' do
+        expect { subject }.to raise_error
+      end
+    end
+
+    context 'when configure hostname' do
+      let(:host) { 'example.com' }
+
+      before do
+        Kinu.configure do |config|
+          config.host = host
+        end
+      end
+
+      it 'return http url' do
+        is_expected.to eq(URI::HTTP.build(host: host))
+      end
+    end
+
+    context 'when configure hostname and ssl' do
+      let(:host) { 'example.com' }
+      let(:ssl) { true }
+
+      before do
+        Kinu.configure do |config|
+          config.host = host
+          config.ssl = ssl
+        end
+      end
+
+      it 'returns https url' do
+        is_expected.to eq(URI::HTTPS.build(host: host))
+      end
+    end
   end
 end
